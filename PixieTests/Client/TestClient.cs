f2cc8d7ab70b55bf9ed0eecfdc43fc53d2263097
Vyacheslav Vodyanov <crossdow@gmail.com>
2020-01-18 22:12:23 +0400

    Added test client ssl connection feature Added test client builder to simplify test client configuration

--- PixieTests/Client/TestClient.cs
+++ PixieTests/Client/TestClient.cs
@@ -2,32 +2,69 @@
 using System.Net.Sockets;
 using Pixie.Core.Messages;
 using System.IO;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Authentication;
 
 namespace PixieCoreTests.Client
 {
     public class TestClient
     {
+        public class Builder
+        {
+            private string host;
+            private int port;
+            private bool ssl = false;
+            private Type[] eventTypes = new Type[] { };
+            private Action<object> onMessageRecived = null;
+
+            private Builder() { }
+
+            public Builder Host(string host) { this.host = host; return this; }
+            public Builder Port(int port) { this.port = port; return this; }
+            public Builder SslEnabled(bool ssl) { this.ssl = ssl; return this; }
+            public Builder EventTypes(Type[] types) { this.eventTypes = types; return this; }
+            public Builder OnMessageReceived(Action<object> onMessageRecived) { this.onMessageRecived = onMessageRecived; return this; }
+
+            //required parameters
+            public static Builder Create(string host, int port) {
+                return new Builder().Host(host).Port(port);
+            }
+
+            public TestClient Build() {
+                return new TestClient(
+                    this.host, 
+                    this.port, 
+                    this.ssl, 
+                    this.eventTypes, 
+                    this.onMessageRecived
+                );
+            }
+        }
+
         private TcpClient connection = null;
         private Stream stream = null;
         private PXMessageReader reader = null;
         private PXMessageWriter writer = null;
+        private bool ssl = false;
 
         private Type[] eventTypes;
         private string host;
         private int port;
         private Action<object> onMessageRecived;
 
-        public TestClient(string host, int port, Type[] eventTypes, Action<object> onMessageRecived) {
+        public TestClient(string host, int port, bool ssl, Type[] eventTypes, Action<object> onMessageRecived) {
             this.eventTypes = eventTypes;
             this.host = host;
             this.port = port;
+            this.ssl = ssl;
             this.onMessageRecived = onMessageRecived;
         }
 
         public void Run() {
             try {
                 connection = new TcpClient(this.host, this.port);
-                stream = connection.GetStream();
+                stream = WrapSslIfNeeded(connection.GetStream());
                 reader = new PXMessageReader(stream, eventTypes);
                 writer = new PXMessageWriter(stream);
 
@@ -42,6 +79,43 @@ namespace PixieCoreTests.Client
                 Dismiss();
                 throw e;
             }
+        }
+
+        private static bool ValidateServerCertificate(
+              object sender,
+              X509Certificate certificate,
+              X509Chain chain,
+              SslPolicyErrors sslPolicyErrors) {
+            return true;
+        }
+
+        private Stream WrapSslIfNeeded(Stream underlyingStream) {
+            if (!ssl) {
+                return underlyingStream;
+            }
+
+            SslStream sslStream = new SslStream(
+                underlyingStream,
+                false,
+                new RemoteCertificateValidationCallback(ValidateServerCertificate),
+                null
+            );
+
+            // The server name must match the name on the server certificate.
+            try {
+                sslStream.AuthenticateAsClient(this.host);
+            } catch (AuthenticationException e) {
+                Console.WriteLine("Exception: {0}", e.Message);
+                if (e.InnerException != null) {
+                    Console.WriteLine("Inner exception: {0}", e.InnerException.Message);
+                }
+                Console.WriteLine("Authentication failed - closing the connection.");
+                this.connection.Close();
+
+                throw e;
+            }
+
+            return sslStream;
         }
 
         private void Dismiss() {
