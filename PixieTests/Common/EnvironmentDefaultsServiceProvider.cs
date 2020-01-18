2afdf0b0b5a0435c902a2b688f578ff0c7f8846a
Vyacheslav Vodyanov <crossdow@gmail.com>
2020-01-18 22:14:44 +0400

    Improved environment defaults service provider by making it able to setup hostname and port

--- PixieTests/Common/EnvironmentDefaultsServiceProvider.cs
+++ PixieTests/Common/EnvironmentDefaultsServiceProvider.cs
@@ -3,6 +3,7 @@ using Moq;
 using Pixie.Core.ServiceProviders;
 using Pixie.Core.Services;
 using Pixie.Core.Services.Internal;
+using Pixie.Toolbox.StreamWrappers;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,21 +12,31 @@ namespace PixieTests.Common
 {
     internal class EnvironmentDefaultsServiceProvider : IPXServiceProvider
     {
-        public const string HOST = "localhost";
-        public const int PORT = 7777;
+        private string host;
+        private int port;
+
+        internal EnvironmentDefaultsServiceProvider(string host = "localhost", int port = 7777) {
+            this.host = host;
+            this.port = port;
+        }
 
         public void OnBoot(IContainer container) {
             container.RegisterDelegate(delegate(IResolverContext context) {
                 var environmentMock = new Mock<IPXEnvironmentService>();
 
-                environmentMock.Setup(e => e.GetString(It.Is<string>(s => s == PXEnvironmentService.ENV_PARAM_HOST), It.IsAny<Func<string>>())).Returns(HOST);
-                environmentMock.Setup(e => e.GetInt(It.Is<string>(s => s == PXEnvironmentService.ENV_PARAM_PORT), It.IsAny<Func<int>>())).Returns(PORT);
+                environmentMock.Setup(e => e.GetString(It.Is<string>(s => s == PXEnvironmentService.ENV_PARAM_HOST), It.IsAny<Func<string>>())).Returns(this.host);
+                environmentMock.Setup(e => e.GetInt(It.Is<string>(s => s == PXEnvironmentService.ENV_PARAM_PORT), It.IsAny<Func<int>>())).Returns(this.port);
+
+                HandleMock(environmentMock);
 
                 return environmentMock.Object;
             }, ifAlreadyRegistered: IfAlreadyRegistered.Replace);
         }
 
         public void OnPostBoot(IContainer container) {
+        }
+
+        public virtual void HandleMock(Mock<IPXEnvironmentService> mock) {
         }
     }
 }
