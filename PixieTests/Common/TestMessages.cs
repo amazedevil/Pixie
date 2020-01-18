c71fda5c60aff71c9ffaa605eae602938720b0b5
Vyacheslav Vodyanov <crossdow@gmail.com>
2020-01-18 22:10:32 +0400

    Added common test messages keeper

---
+++ PixieTests/Common/TestMessages.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PixieTests.Common
+{
+    internal class TestMessages
+    {
+        public struct InnerTestMessageStruct
+        {
+            public int testInt;
+            public string testString;
+        }
+
+        public struct TestMessageType1
+        {
+            public int testInt;
+            public string testString;
+            public InnerTestMessageStruct testStruct;
+        }
+
+        public static TestMessageType1 TestMessageType1Sample1() {
+            return new TestMessageType1() {
+                testInt = 1,
+                testString = "testMessageString",
+                testStruct = new InnerTestMessageStruct() {
+                    testInt = 2,
+                    testString = "testInternalMessageStructTest"
+                }
+            };
+        }
+    }
+}
