cbc018453cd56b53dc00e0cd52ed8ebefe7c8e56
Vyacheslav Vodyanov <crossdow@gmail.com>
2020-01-18 22:09:06 +0400

    Added port provider for tests to prevent ports collision between multiple running tests

---
+++ PixieTests/Common/PortProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PixieTests.Common
+{
+    internal static class PortProvider
+    {
+        private static int counter = 7777;
+
+        public static int ProviderPort() {
+            return counter++;
+        }
+    }
+}
