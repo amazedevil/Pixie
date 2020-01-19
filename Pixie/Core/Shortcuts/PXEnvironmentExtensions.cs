66bad458ef75a4ec264d5730d7c23dbe78923742
Vyacheslav Vodyanov <crossdow@gmail.com>
2020-01-20 01:30:09 +0400

    Made host and port public, cause this information might be useful for user-project

--- Pixie/Core/Shortcuts/PXEnvironmentExtensions.cs
+++ Pixie/Core/Shortcuts/PXEnvironmentExtensions.cs
@@ -1,19 +1,16 @@
 ﻿using Pixie.Core.Exceptions;
 using Pixie.Core.Services;
 using Pixie.Core.Services.Internal;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
-public  static class PXEnvironmentExtensions
+public static class PXEnvironmentExtensions
 {
-    internal static string Host(this IPXEnvironmentService service) {
+    public static string Host(this IPXEnvironmentService service) {
         return service.GetString(PXEnvironmentService.ENV_PARAM_HOST, delegate {
             throw new PXRequiredEnvironmentParameterNotFound(PXEnvironmentService.ENV_PARAM_HOST);
         });
     }
 
-    internal static int Port(this IPXEnvironmentService service) {
+    public static int Port(this IPXEnvironmentService service) {
         return service.GetInt(PXEnvironmentService.ENV_PARAM_PORT, delegate {
             throw new PXRequiredEnvironmentParameterNotFound(PXEnvironmentService.ENV_PARAM_PORT);
         });
