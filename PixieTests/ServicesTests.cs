1aacab26b3f8cf23a7ab64b181f5b16c83ac2542
Vyacheslav Vodyanov <crossdow@gmail.com>
2020-01-27 22:42:52 +0400

    Added common services avilability test

---
+++ PixieTests/ServicesTests.cs
@@ -0,0 +1,43 @@
+﻿using DryIoc;
+using NUnit.Framework;
+using Pixie.Core;
+using Pixie.Core.ServiceProviders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PixieTests
+{
+    class ServicesTests {
+        private class TestServiceProvider : IPXServiceProvider
+        {
+            public void OnBoot(IContainer container) {
+            }
+
+            public void OnPostBoot(IContainer container) {
+                Assert.IsNotNull(container.Logger());
+                Assert.IsNotNull(container.Middlewares());
+                Assert.IsNotNull(container.Env());
+                Assert.IsNotNull(container.Client());
+                Assert.IsNotNull(container.Scheduler());
+                Assert.IsNotNull(container.Sender());
+                Assert.IsNotNull(container.Handlers());
+                Assert.IsNotNull(container.Errors());
+            }
+        }
+
+        private class TestServer : PXServer
+        {
+            protected override IPXServiceProvider[] GetServiceProviders() {
+                return new IPXServiceProvider[] {
+                    new TestServiceProvider(),
+                };
+            }
+        }
+
+        [Test]
+        public void CommonServicesAvailabilityTest() {
+            (new TestServer()).StartAsync();
+        }
+    }
+}
