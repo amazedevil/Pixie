5939fbf6f66156d3d2604ab826404efd396b8f3f
Vyacheslav Vodyanov <crossdow@gmail.com>
2020-02-28 01:41:12 +0400

    Fixed unused environment shortcuts

--- Pixie/Core/Shortcuts/PXEnvironmentExtensions.cs
+++ Pixie/Core/Shortcuts/PXEnvironmentExtensions.cs
@@ -4,18 +4,6 @@ using Pixie.Core.Services.Internal;
 
 public static class PXEnvironmentExtensions
 {
-    public static string Host(this IPXEnvironmentService service) {
-        return service.GetString(PXEnvironmentService.ENV_PARAM_HOST, delegate {
-            throw new PXRequiredEnvironmentParameterNotFound(PXEnvironmentService.ENV_PARAM_HOST);
-        });
-    }
-
-    public static int Port(this IPXEnvironmentService service) {
-        return service.GetInt(PXEnvironmentService.ENV_PARAM_PORT, delegate {
-            throw new PXRequiredEnvironmentParameterNotFound(PXEnvironmentService.ENV_PARAM_PORT);
-        });
-    }
-
     public static string GetString(this IPXEnvironmentService service, string key, string defaultValue = null) {
         return service.GetString(key, delegate { return defaultValue; });
     }
