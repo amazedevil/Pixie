b8d17f520bdf54d5ea1766fb6fc85e2ceee7786a
Vyacheslav Vodyanov <crossdow@gmail.com>
2020-03-17 03:05:13 +0400

    Added agents test

--- PixieTests/ServerTests.cs
+++ PixieTests/ServerTests.cs
@@ -99,5 +99,98 @@ namespace PixieTests
 
             dataReceivedEvent.Reset();
         }
+
+        //Agent message passing test
+        private class AgentPassingTestServer : PXServer, IPXServiceProvider
+        {
+            private class ActionExecutorMessageHandler : PXMessageHandlerBase<TestMessages.TestMessageType1>
+            {
+                private Action<TestMessages.TestMessageType1> action;
+
+                public ActionExecutorMessageHandler(Action<TestMessages.TestMessageType1> action) {
+                    this.action = action;
+                }
+
+                public override void Handle(TestMessages.TestMessageType1 data) {
+                    this.action(data);
+                }
+            }
+
+            private ActionExecutorMessageHandler handler;
+            private string address;
+            private int port;
+            private string remoteAddress;
+            private int remotePort;
+
+            internal AgentPassingTestServer(Action<TestMessages.TestMessageType1> action, string address, int port, string remoteAddress, int remotePort) : base() {
+                if (action != null) {
+                    this.handler = new ActionExecutorMessageHandler(action);
+                } else {
+                    this.handler = null;
+                }
+                this.address = address;
+                this.port = port;
+                this.remoteAddress = remoteAddress;
+                this.remotePort = remotePort;
+            }
+
+            protected override IPXServiceProvider[] GetServiceProviders() {
+                return new IPXServiceProvider[] {
+                    this,
+                };
+            }
+
+            public void Send(object message) {
+                this.container.AgentSender().Send(null, message);
+            }
+
+            public void OnRegister(IContainer container) {
+                container.Endpoints().RegisterSockerServer(this.address, this.port);
+
+                if (this.remoteAddress != default) {
+                    container.Agents().RegisterAgent(remoteAddress, remotePort);
+                }
+
+                if (handler != null) {
+                    container.Handlers().Register(PXHandlerService.MessageHandlerItem.CreateWithProvider(delegate { return handler; }));
+                }
+            }
+
+            public void OnInitialize(IContainer container) {
+            }
+        }
+
+
+        [Test]
+        public void AgentMessageTest() {
+            var receiverServerPort = PortProvider.ProviderPort();
+            var senderServerPort = PortProvider.ProviderPort();
+
+            var messageToSend = TestMessages.TestMessageType1Sample1();
+
+            ManualResetEvent dataReceivedEvent = new ManualResetEvent(false);
+            AgentPassingTestServer receiverServer = new AgentPassingTestServer(delegate(TestMessages.TestMessageType1 message) {
+                Assert.AreEqual(messageToSend, message);
+                dataReceivedEvent.Set();
+            }, "0.0.0.0", receiverServerPort, default, 0);
+
+            AgentPassingTestServer senderServer = new AgentPassingTestServer(
+                null, 
+                "0.0.0.0", 
+                senderServerPort, 
+                "127.0.0.1", 
+                receiverServerPort
+            );
+
+            receiverServer.StartAsync();
+            senderServer.StartAsync();
+            senderServer.Send(messageToSend);
+
+            if (!dataReceivedEvent.WaitOne(5000)) {
+                Assert.Fail("Message timeout");
+            }
+
+            dataReceivedEvent.Reset();
+        }
     }
 }
