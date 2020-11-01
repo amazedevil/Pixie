deda6c1afcef39c9e852b2526def0a204d8b8fd3
Vyacheslav Vodyanov <crossdow@gmail.com>
2020-11-01 15:12:09 +0400

    Added some sender dispatcher specific exceptions wrapping for better debugging

--- Pixie/Core/Services/PXSenderDispatcherService.cs
+++ Pixie/Core/Services/PXSenderDispatcherService.cs
@@ -1,10 +1,19 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 
 namespace Pixie.Core.Services
 {
     public class PXSenderDispatcherService
     {
+        public class SenderNotFoundException : Exception {
+            public SenderNotFoundException(int senderId) : base($"Sender with id \"${senderId}\" not found") { }
+        }
+
+        public class DefaultSenderNotFoundException : Exception {
+            public DefaultSenderNotFoundException() : base("Default sender not found: probably no server is running") { }
+        }
+
         public const int DEFAULT_SERVER_SENDER_ID = 1;
         public const int DEFAULT_AGENT_SENDER_ID = 2;
 
@@ -21,11 +30,19 @@ namespace Pixie.Core.Services
         }
 
         public IPXMessageSenderService GetSender(int senderId) {
-            return senders[senderId];
+            try {
+                return senders[senderId];
+            } catch (KeyNotFoundException) {
+                throw new SenderNotFoundException(senderId);
+            }
         }
 
         public IPXMessageSenderService GetDefaultSender() {
-            return GetSender(DEFAULT_SERVER_SENDER_ID);
+            try {
+                return GetSender(DEFAULT_SERVER_SENDER_ID);
+            } catch (SenderNotFoundException) {
+                throw new DefaultSenderNotFoundException();
+            }
         }
 
         public IPXMessageSenderService GetDefaultAgentSender() {
